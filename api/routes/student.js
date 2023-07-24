a4f27b020d2e6456c54512acb07a83679db99155
Shaikh juned <sheikhjuned770@gmail.com>
2023-07-24 13:14:48 +0530

    GET Reauest Complliyt

--- api/routes/student.js
+++ api/routes/student.js
@@ -1,36 +1,44 @@
 const express = require("express");
 const router = express.Router();
-const Student = require('../model/studentSchema')
-const mongoose = require('mongoose')
+const Student = require("../model/studentSchema");
+const mongoose = require("mongoose");
 
 router.get("/", (req, res) => {
-  res.status(200).json({
-    msg: "this is student get request",
-  });
-});
-
-router.post("/", (req, res, next) => {
-  const student = new Student({
-    _id:new mongoose.Types.ObjectId,
-    name : req.body.name,
-    email:req.body.email,
-    phone:req.body.phone,
-    gender:req.body.gender
-  })
-  student.save()
-  .then(result=>{
-    console.log(result);
+  Student.find().then((result) => {
     res.status(200).json({
-      newStudent:result
-    })
+      studentData: result,
+    });
   })
-  .catch(err=>{
-    console.log(err);
+  .then(err=>{
+    console.log(err)
     res.status(500).json({
       error:err
     })
   })
-}); 
+});
+
+router.post("/", (req, res, next) => {
+  const student = new Student({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    gender: req.body.gender,
+  });
+  student
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        newStudent: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 module.exports = router;
- 
